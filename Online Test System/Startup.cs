2b9e8c2a0077a8e644f56c76d0238a6d9a46ef12
Hussain.Morawala <Hussain.Morawala@IBRD343>
2021-02-21 17:55:58 +0530

    Add project files.

---
+++ Online Test System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Test_System.Startup))]
+namespace Online_Test_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
